8ff84b6e979d0870d212938b8abecb30343f1b4e
SondosMuhammed887 <sondos.farag05@eng-st.cu.edu.eg>
2026-03-17 12:18:20 +0200

    Add files via upload

---
+++ Task 2.c
@@ -0,0 +1,20 @@
+/*
+ * Task 2.c
+ 
+ */ 
+
+#include <avr/io.h>
+
+
+void ToggleBit(unsigned int *num, int bit)
+{
+	*num ^= (1 << bit);
+}
+
+int main(void)
+{
+	unsigned int x = 5;  // 0101
+	ToggleBit(&x, 1);
+
+
+}
